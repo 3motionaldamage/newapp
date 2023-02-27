d8020f5f31421ef872f4a721a786ff63849c2f70
3motionaldamage <105239587+3motionaldamage@users.noreply.github.com>
2023-02-27 23:55:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 # create a sidebar menu
 menu = ["Dashboard", "Members", "Classes", "Bookings"]
@@ -6,30 +7,50 @@ choice = st.sidebar.selectbox("Select a page", menu)
 
 # create a function for the dashboard page
 def dashboard():
-  st.title("Gym Management Dashboard")
-  # include relevant information about the gym's performance, such as membership numbers, revenue, etc.
+    st.title("Gym Management Dashboard")
+    # include relevant information about the gym's performance, such as membership numbers, revenue, etc.
 
 # create a function for the members page
 def members():
-  st.title("Gym Members")
-  # include a table displaying the gym's members, their membership details, and any other relevant information.
+    st.title("Gym Members")
+
+    # display a form for adding a new member
+    st.subheader("Add New Member")
+    name = st.text_input("Name")
+    email = st.text_input("Email")
+    membership_start = st.date_input("Membership Start Date")
+    membership_end = st.date_input("Membership End Date")
+    submit = st.button("Add Member")
+
+    # when the user submits the form, add the new member to the data file
+    if submit:
+        new_member = {"Name": name, "Email": email, "Membership Start Date": membership_start, "Membership End Date": membership_end}
+        members_df = pd.read_csv("members.csv")
+        members_df = members_df.append(new_member, ignore_index=True)
+        members_df.to_csv("members.csv", index=False)
+        st.success("Member added!")
+
+    # display a table of all members
+    members_df = pd.read_csv("members.csv")
+    st.subheader("Current Members")
+    st.dataframe(members_df)
 
 # create a function for the classes page
 def classes():
-  st.title("Gym Classes")
-  # include a table displaying the gym's class schedule, including the class name, time, and location.
+    st.title("Gym Classes")
+    # include a table displaying the gym's class schedule, including the class name, time, and location.
 
 # create a function for the bookings page
 def bookings():
-  st.title("Gym Bookings")
-  # include a table displaying the gym's current bookings, including the member name, class name, and booking time.
+    st.title("Gym Bookings")
+    # include a table displaying the gym's current bookings, including the member name, class name, and booking time.
 
 # use conditional statements to display the appropriate page based on user choice
 if choice == "Dashboard":
-  dashboard()
+    dashboard()
 elif choice == "Members":
-  members()
+    members()
 elif choice == "Classes":
-  classes()
+    classes()
 elif choice == "Bookings":
-  bookings()
+    bookings()
