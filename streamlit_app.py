3d556918b8c021fdf6b343e4b6388e1cde0e1bed
3motionaldamage <105239587+3motionaldamage@users.noreply.github.com>
2023-02-28 12:42:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,40 +8,52 @@ def main():
     page_bg_img = '''
     <style>
     .stApp {
-      background-image: url("https://i.pinimg.com/originals/0e/39/c2/0e39c256714a4b7c0299323a82701647.jpg");
+      background-image: url("https://cdn.pixabay.com/photo/2016/11/29/03/20/background-1869755_960_720.png");
       background-size: cover;
     }
     </style>
     '''
     st.markdown(page_bg_img, unsafe_allow_html=True)
-    
+
+    # Set navigation bars
+    st.sidebar.title("Navigation")
+    pages = ["Home", "Skills and Experiences", "Look At My Work"]
+    selection = st.sidebar.radio("Go to", pages)
+
     # Set profile picture
-    st.image("https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png", width=250)
-    
+    st.image("https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png", width=250, 
+             caption="My Profile Picture", use_column_width=False,
+             output_format="PNG", style="display: block; margin: auto;")
+
     # Set page title and subtitle
     st.title("My Portfolio Website")
     st.write("Welcome to my portfolio website!")
-    
-    # Add content
-    st.header("My Projects")
-    st.write("""
-    - Project 1
-    - Project 2
-    - Project 3
-    """)
-    
-    st.header("My Skills")
-    st.write("""
-    - Skill 1
-    - Skill 2
-    - Skill 3
-    """)
-    
+
+    # Show selected page content
+    if selection == "Home":
+        st.header("Home")
+        st.write("""
+        This is the home page. Here, you can add any content you want to share with visitors to your website.
+        """)
+
+    elif selection == "Skills and Experiences":
+        st.header("Skills and Experiences")
+        st.write("""
+        This page is where you can share information about your skills and experiences.
+        """)
+
+    elif selection == "Look At My Work":
+        st.header("Look At My Work")
+        st.write("""
+        This page is where you can showcase your work and projects.
+        """)
+
+    # Add contact information
     st.header("Contact Me")
     st.write("""
     - Email: me@example.com
     - Phone: 123-456-7890
     """)
-    
+
 if __name__ == "__main__":
     main()
