05af7d862f56b643d6556ff1892ed990c99ae816
3motionaldamage <105239587+3motionaldamage@users.noreply.github.com>
2023-02-28 10:22:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,56 +1,28 @@
 import streamlit as st
-import pandas as pd
 
-# create a sidebar menu
-menu = ["Dashboard", "Members", "Classes", "Bookings"]
-choice = st.sidebar.selectbox("Select a page", menu)
-
-# create a function for the dashboard page
-def dashboard():
-    st.title("Gym Management Dashboard")
-    # include relevant information about the gym's performance, such as membership numbers, revenue, etc.
-
-# create a function for the members page
-def members():
-    st.title("Gym Members")
-
-    # display a form for adding a new member
-    st.subheader("Add New Member")
-    name = st.text_input("Name")
-    email = st.text_input("Email")
-    membership_start = st.date_input("Membership Start Date")
-    membership_end = st.date_input("Membership End Date")
-    submit = st.button("Add Member")
-
-    # when the user submits the form, add the new member to the data file
-    if submit:
-        new_member = {"Name": name, "Email": email, "Membership Start Date": membership_start, "Membership End Date": membership_end}
-        members_df = pd.read_csv("members.csv")
-        members_df = members_df.append(new_member, ignore_index=True)
-        members_df.to_csv("members.csv", index=False)
-        st.success("Member added!")
-
-    # display a table of all members
-    members_df = pd.read_csv("members.csv")
-    st.subheader("Current Members")
-    st.dataframe(members_df)
-
-# create a function for the classes page
-def classes():
-    st.title("Gym Classes")
-    # include a table displaying the gym's class schedule, including the class name, time, and location.
-
-# create a function for the bookings page
-def bookings():
-    st.title("Gym Bookings")
-    # include a table displaying the gym's current bookings, including the member name, class name, and booking time.
-
-# use conditional statements to display the appropriate page based on user choice
-if choice == "Dashboard":
-    dashboard()
-elif choice == "Members":
-    members()
-elif choice == "Classes":
-    classes()
-elif choice == "Bookings":
-    bookings()
+def main():
+    st.title("My Portfolio Website")
+    st.write("Welcome to my portfolio website!")
+    
+    st.header("My Projects")
+    st.write("""
+    - Project 1
+    - Project 2
+    - Project 3
+    """)
+    
+    st.header("My Skills")
+    st.write("""
+    - Skill 1
+    - Skill 2
+    - Skill 3
+    """)
+    
+    st.header("Contact Me")
+    st.write("""
+    - Email: me@example.com
+    - Phone: 123-456-7890
+    """)
+    
+if __name__ == "__main__":
+    main()
